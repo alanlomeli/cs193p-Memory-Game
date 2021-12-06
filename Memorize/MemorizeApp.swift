4f9063c9c546de007253749611bb59581eff3b3f
yisusalanpng <45440937+yisusalanpng@users.noreply.github.com>
2021-12-05 22:14:49 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alan Lomeli on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
